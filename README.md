f51a766bbe57206d54592ce0c5eb3b1708fae84c
belarge <bruce.large@gmail.com>
2015-07-25 21:05:21 +1000

    Data Science Gathering Assignment Final Push

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# datascience_wearablecomputing
-Coursera Getting and Cleaning Data - Wearable Computing Assignment
-# Another Title
-## More Titles
+# datascience_wearablecomputing 
+#Coursera Getting and Cleaning Data - Wearable Computing Assignment
+
+This script reads in the UCI HAR Dataset to perform analytics regarding Wearable Computing. 
+
+The data required for this script can be found from - http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+The output variables are:
+
+SubjectID - Number from 1 to 30       
+Activity - String with Values [LAYING, SITTING, STANDING, WALKING, WALKING_DOWNSTAIRS, WALKING_UPSTAIRS]
+Mean (_Mean) and Standard Deviations (_StDev) for the below Time Domain Variables
+ tBodyAccMag
+ tGravityAccMag
+ tBodyAccJerkMag
+ tBodyGyroMag
+ tBodyGyroJerkMag
+Mean (_Mean) and Standard Deviations (_StDev) for the below Frequency Domain Variables
+ fBodyAccMag
+ fBodyBodyAccJerkMag
+ fBodyBodyGyroMag_Mean
+ fBodyBodyGyroJerkMag
+ 
--- run_analysis.R
+++ run_analysis.R
@@ -2,26 +2,28 @@
 # Read in Activity Codes
 library(plyr)
 library(dplyr)
+#Read in Activity Codes Keys and Descriptions
 ActivityCodes <- read.table("UCI HAR Dataset/activity_labels.txt",col.names = c("ActivityNumber","Activity"))
+#Read in Feature Codes Keys and Descriptions
 FeatureCodes <- read.table("UCI HAR Dataset/features.txt",col.names = c("FeatureIndex","FeatureName"), stringsAsFactors=FALSE)
 
 #Read in Test Data Set
 testData <- read.table("UCI HAR Dataset/test/X_test.txt",col.names=FeatureCodes$FeatureName)
 testDataSubject <- read.table("UCI HAR Dataset/test/subject_test.txt",col.names="SubjectID")
 testDataActNum <- read.table("UCI HAR Dataset/test/y_test.txt",col.names="ActivityNumber")
+#combine together all test data with Subject ID and Activity Names
 testDataCombined <- cbind(testDataSubject,testDataActNum,testData)
-dim(testDataCombined)
+#dim(testDataCombined)
 
 
 #read in Train Data Set
 trainData <- read.table("UCI HAR Dataset/train/X_train.txt",col.names=FeatureCodes$FeatureName)
 trainDataSubject <- read.table("UCI HAR Dataset/train/subject_train.txt",col.names="SubjectID")
 trainDataActNum <- read.table("UCI HAR Dataset/train/y_train.txt",col.names="ActivityNumber")
+#combine together all train data with Subject ID and Activity Names
 trainDataCombined <- cbind(trainDataSubject,trainDataActNum,trainData)
-#testDataWithActNum <- cbind(testDataSubject,testDataActNum,testData)
-dim(trainDataCombined)
 
-
+#dim(trainDataCombined)
 
 combinedData <- rbind(testDataCombined,trainDataCombined)
 combinedData <- join(ActivityCodes,combinedData)
@@ -30,5 +32,17 @@ combinedData <- join(ActivityCodes,combinedData)
 
 combinedDataCleaned <- select(combinedData,matches("subjectid|mean|std|Activity$"))
 names(combinedDataCleaned)
-aTidyDataSet <- ddply(combinedDataCleaned,.(SubjectID,Activity),summarise,tBodyAcc_Mean_X=mean(tBodyAcc.mean...X),tBodyAcc_Mean_Y=mean(tBodyAcc.mean...Y),tBodyAcc_Mean_Z=mean(tBodyAcc.mean...Z))
+
+aTidyDataSet <- ddply(combinedDataCleaned,.(SubjectID,Activity),summarise,
+                      tBodyAccMag_Mean=mean(tBodyAccMag.mean..),tBodyAccMag_StDev=mean(tBodyAccMag.std..),
+                      tGravityAccMag_Mean=mean(tGravityAccMag.mean..),tGravityAccMag_StDev=mean(tGravityAccMag.std..),
+                      tBodyAccJerkMag_Mean=mean(tBodyAccJerkMag.mean..),tBodyAccJerkMag_StDev=mean(tBodyAccJerkMag.std..),
+                      tBodyGyroMag_Mean=mean(tBodyGyroMag.mean..),tBodyGyroMag_StDev=mean(tBodyGyroMag.std..),
+                      tBodyGyroJerkMag_Mean=mean(tBodyGyroJerkMag.mean..),tBodyGyroJerkMag_StDev=mean(tBodyGyroJerkMag.std..),
+                  
+                      fBodyAccMag_Mean=mean(fBodyAccMag.mean..),fBodyAccMag_StDev=mean(fBodyAccMag.std..),
+                      fBodyBodyAccJerkMag_Mean=mean(fBodyBodyAccJerkMag.mean..),fBodyBodyAccJerkMag_StDev=mean(fBodyBodyAccJerkMag.std..),
+                      fBodyBodyGyroMag_Mean=mean(fBodyBodyGyroMag.mean..),fBodyBodyGyroMag_StDev=mean(fBodyBodyGyroMag.std..),
+                      fBodyBodyGyroJerkMag_Mean=mean(fBodyBodyGyroJerkMag.mean..),fBodyBodyGyroJerkMag_StDev=mean(fBodyBodyGyroJerkMag.std..)
+)
 write.table(aTidyDataSet,row.name=FALSE,file="wearableComputingCleanDataOutput.txt")
